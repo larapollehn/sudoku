04ea901404450c814dd3791ea061bb7973f66d35
LarryWho11 <lara.pollehn@googlemail.com>
2020-05-25 13:12:27 +0200

    wip

--- src/Controller/SudokuController.ts
+++ src/Controller/SudokuController.ts
@@ -1,18 +1,29 @@
 import SudokuViewPuzzle from "../View/SudokuViewPuzzle";
 import SudokuViewMenu from "../View/SudokuViewMenu";
+import Generator from "../Sudoku/Generator";
 
 export default class SudokuController{
     private puzzleView: SudokuViewPuzzle;
-    private menuView: SudokuViewMenu;
+    private Generator: Generator = new Generator();
 
     constructor() {
         this.puzzleView = new SudokuViewPuzzle();
-        this.menuView = new SudokuViewPuzzle();
+
+        this.addEventListener = this.addEventListener.bind(this);
+        this.generateSudoku = this.generateSudoku.bind(this);
     }
 
     setup(){
-        this.puzzleView.initialize();
-        this.menuView.initialize();
+        this.addEventListener();
     }
 
+    addEventListener(){
+        this.puzzleView.generateBtn.addEventListener('click', this.generateSudoku);
+    }
+
+    generateSudoku(){
+        console.log('BITCH SOMEONE TICKLED MY ASS');
+        let sudoku = this.Generator.generateSudoku(30);
+        this.puzzleView.displaySudoku(sudoku);
+    }
 }
--- src/View/SudokuViewPuzzle.ts
+++ src/View/SudokuViewPuzzle.ts
@@ -1,9 +1,22 @@
+import Square from "../Sudoku/Square";
+
 export default class SudokuViewPuzzle{
+    public sudokuList: HTMLElement = document.getElementById('sudokuList');
+    public generateBtn: HTMLElement = document.getElementById('generateBtn');
 
     constructor() {
+        this.displaySudoku = this.displaySudoku.bind(this);
     }
 
-    initialize(){
-
+    displaySudoku(sudoku: Array<Square>){
+        sudoku.forEach(square => {
+            let li = document.createElement('li');
+            li.classList.add('sudokuLiElement');
+            let span = document.createElement('span');
+            span.innerText = String(square.value);
+            li.appendChild(span);
+            this.sudokuList.appendChild(li);
+        })
     }
+
 }
