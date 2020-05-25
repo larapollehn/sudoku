3c171102844cc44ce09e09d56910e6af645a26d2
LarryWho11 <lara.pollehn@googlemail.com>
2020-05-25 13:19:31 +0200

    displaying the sudoku

--- src/View/SudokuViewPuzzle.ts
+++ src/View/SudokuViewPuzzle.ts
@@ -1,6 +1,6 @@
 import Square from "../Sudoku/Square";
 
-export default class SudokuViewPuzzle{
+export default class SudokuViewPuzzle {
     public sudokuList: HTMLElement = document.getElementById('sudokuList');
     public generateBtn: HTMLElement = document.getElementById('generateBtn');
 
@@ -8,12 +8,19 @@ export default class SudokuViewPuzzle{
         this.displaySudoku = this.displaySudoku.bind(this);
     }
 
-    displaySudoku(sudoku: Array<Square>){
+    displaySudoku(sudoku: Array<Square>) {
+        while (this.sudokuList.hasChildNodes()) {
+            this.sudokuList.removeChild(this.sudokuList.firstChild);
+        }
+
         sudoku.forEach(square => {
             let li = document.createElement('li');
             li.classList.add('sudokuLiElement');
+            li.id = String(square.index);
             let span = document.createElement('span');
-            span.innerText = String(square.value);
+            if (square.value !== 0) {
+                span.innerText = String(square.value);
+            }
             li.appendChild(span);
             this.sudokuList.appendChild(li);
         })
